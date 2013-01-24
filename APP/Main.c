4ee3aac4c35f4dc3c80fab33c3ea99dab4c8b998
zhouq3132@gmail.com <zhouq3132@gmail.com@b410fea8-01d2-c24c-cfab-e52b0a45e8ad>
2013-01-24 05:43:40 +0000

    将uIP的任务独立出来，调整代码的文件结构，准备添加uIP主动发送支持：uIP网络任务等待消息邮箱的消息，网络收发放到IDLE任务中完成，收到数据后发送消息，按键发生也发送消息，uIP任务根据不同的消息进行不同处理。

--- APP/Main.c
+++ APP/Main.c
@@ -9,86 +9,16 @@
 #include "tapdev.h"
 #include "clock-arch.h"
 
+/* task priority */ 
+#define STARTUP_TASK_PRIO     4
+#define NET_TASK_PRIO         6
+
+/* task stack size */ 
+#define STARTUP_TASK_STK_SIZE 80  
+#define NET_TASK_STK_SIZE     1024
+
 static OS_STK  Startup_Task_STK[STARTUP_TASK_STK_SIZE];
 static OS_STK  Net_Task_STK[NET_TASK_STK_SIZE];
-volatile int32_t uIP_RunTime = 0;//__IO == volatile
-OS_EVENT *WIFI_INT_Mbox;
-
-
-static void Net_Task(void* p_arg)
-{
-	FlagStatus *WIFI_INT;
-	BOOL bStatus = FALSE;
-	
-	//
-	// Wifi start!
-	//
-	WIFI_INT_Mbox = OSMboxCreate((void *)0);
-	bStatus = Wlan_Init(g_pWlanAdapter);
-	if (!bStatus) {
-		TRACE("Wlan init failed!!\r\n");
-		return;
-	}
-
-	if (!Get_KEY1()) { // 启动时KEY1按下进入模块的设置模式
-		
-		// uIP Init
-		uip_ipaddr_t ipaddr;
-
-		WlanAdapter_MacAddr(g_pWlanAdapter, uip_ethaddr.addr, FALSE);
-		uip_init();
-		
-		uip_ipaddr(ipaddr, 192, 168, 1, 1);
-		uip_sethostaddr(ipaddr);
-		uip_ipaddr(ipaddr, 192, 168, 1, 1);
-		uip_setdraddr(ipaddr);
-		uip_ipaddr(ipaddr, 255, 255, 255, 0);
-		uip_setnetmask(ipaddr);
-			
-		p_appcall = httpd_appcall;
-		httpd_init();
-			
-		WiFiCmd_RunCommand ("set ssid WEBLED");
-		WiFiCmd_RunCommand ("set networktype adhoc");
-		WiFiCmd_RunCommand ("set authmode open");
-		WiFiCmd_RunCommand ("set encrymode disable");
-		WiFiCmd_RunCommand ("set keyascii 12345");
-	}
-	else{ //work mode,use UDP
-		// uIP Init
-		uip_ipaddr_t ipaddr;
-
-		WlanAdapter_MacAddr(g_pWlanAdapter, uip_ethaddr.addr, FALSE);
-		uip_init();
-		
-    UDP_App_Init();
-
-		uip_ipaddr(ipaddr, 192, 168, 1, 2);
-		uip_sethostaddr(ipaddr);
-		uip_ipaddr(ipaddr, 192, 168, 1, 1);
-		uip_setdraddr(ipaddr);
-		uip_ipaddr(ipaddr, 255, 255, 255, 0);
-		uip_setnetmask(ipaddr);
-
-		WiFiCmd_RunCommand ("set ssid CDDC");
-		WiFiCmd_RunCommand ("set networktype infra");
-		WiFiCmd_RunCommand ("set authmode open");
-		WiFiCmd_RunCommand ("set encrymode wep");
-		WiFiCmd_RunCommand ("set keyascii 12345");
-	}
-	WiFiCmd_RunCommand ("ctrl connect");
-		
-	while(1) {
-			if (WIFI_GET_FLAG(g_pWlanAdapter, FLAG_PKTRCVED)) {
-				WIFI_CLR_FLAG(g_pWlanAdapter, FLAG_PKTRCVED);
-				UipPro();
-			}
-			if (g_Console.ucState & CONSOLE_STATE_RECEIVED) {
-				WiFiCmd_RunCommand ((char*)g_Console.ucConsoleRxBuffer);
-				Console_ResetRx();
-			}
-		}		
-	}
 
 static void Startup_Task(void* p_arg)
 {
