9ac75472aede92412c8f636539d724c7fec52284
zhouq3132@gmail.com <zhouq3132@gmail.com@b410fea8-01d2-c24c-cfab-e52b0a45e8ad>
2013-01-25 09:43:32 +0000

    调整函数结构，可以正常工作，备份

--- APP/uIP_TASK.c
+++ APP/uIP_TASK.c
@@ -22,6 +22,51 @@ void (*p_appcall)(void);
 
 void Net_Task(void* p_arg)
 {
+  Set_uIP();
+  while(1) 
+  {
+    if(WIFI_GET_FLAG(g_pWlanAdapter, FLAG_PKTRCVED)) 
+    {
+      WIFI_CLR_FLAG(g_pWlanAdapter, FLAG_PKTRCVED);
+      uip_len = tapdev_read();    //从网络设备读取一个IP包,返回数据长度
+      if (uip_len > 0)          //收到数据时长度会变化
+      {
+        /* 处理IP数据包(只有校验通过的IP包才会被接收) */
+        if(BUF->type == htons(UIP_ETHTYPE_IP)) 
+        { //收到IP包
+          uip_arp_ipin();        //去除以太网头结构，更新ARP表
+          uip_input();           //IP包处理
+          if (uip_len > 0) 
+          {      //有带外回应数据
+            uip_arp_out();      //加以太网头结构，在主动连接时可能要构造ARP请求
+            tapdev_send();      //发送数据到以太网（设备驱动程序）
+          }
+        }
+        /* 处理arp报文 */
+        else if (BUF->type == htons(UIP_ETHTYPE_ARP)) 
+        { //是ARP请求包
+          uip_arp_arpin();        //如是是ARP回应，更新ARP表；如果是请求，构造回应数据包
+          /*
+          当上面的函数执行后，如果需要发送数据，则全局变量 uip_len > 0
+          需要发送的数据在uip_buf, 长度是uip_len  (这是2个全局变量)
+          */
+          if (uip_len > 0) 
+          {      //是ARP请求，要发送回应
+            tapdev_send();      //发ARP回应到以太网上
+          }
+        }
+      }
+    }
+    if(g_Console.ucState & CONSOLE_STATE_RECEIVED) 
+    {
+      WiFiCmd_RunCommand ((char*)g_Console.ucConsoleRxBuffer);
+      Console_ResetRx();
+    }
+  }		
+}
+
+static void Set_uIP()
+{
   BOOL bStatus = FALSE;
   //
   // Wifi start!
@@ -52,10 +97,10 @@ void Net_Task(void* p_arg)
     p_appcall = httpd_appcall;
     httpd_init();
     
-    WiFiCmd_RunCommand ("set ssid WEBLED");
-    WiFiCmd_RunCommand ("set networktype adhoc");
+    WiFiCmd_RunCommand ("set ssid CDDC");
+    WiFiCmd_RunCommand ("set networktype infra");
     WiFiCmd_RunCommand ("set authmode open");
-    WiFiCmd_RunCommand ("set encrymode disable");
+    WiFiCmd_RunCommand ("set encrymode wep");
     WiFiCmd_RunCommand ("set keyascii 12345");
   }
   else{ //work mode,use UDP
@@ -81,69 +126,4 @@ void Net_Task(void* p_arg)
     WiFiCmd_RunCommand ("set keyascii 12345");
   }
   WiFiCmd_RunCommand ("ctrl connect");
-  
-  while(1) {
-    if (WIFI_GET_FLAG(g_pWlanAdapter, FLAG_PKTRCVED)) {
-      WIFI_CLR_FLAG(g_pWlanAdapter, FLAG_PKTRCVED);
-      UipPro();
-    }
-    if (g_Console.ucState & CONSOLE_STATE_RECEIVED) {
-      WiFiCmd_RunCommand ((char*)g_Console.ucConsoleRxBuffer);
-      Console_ResetRx();
-    }
-  }		
-}
-
-/*******************************************************************************
-*   函数名：UipPro
-*   输  入:
-*   输  出:
-*   功能说明：中断触发读取网络接收缓存
-*   注意：当使用uCOS时，不适合用中断。请务必谨记。
-********************************************************************/
-void UipPro(void)
-{
-  uip_len = tapdev_read();    //从网络设备读取一个IP包,返回数据长度
-  if (uip_len > 0)          //收到数据时长度会变化
-  {
-    /* 处理IP数据包(只有校验通过的IP包才会被接收) */
-    if (BUF->type == htons(UIP_ETHTYPE_IP)) 
-    { //收到IP包
-      uip_arp_ipin();        //去除以太网头结构，更新ARP表
-      uip_input();           //IP包处理
-      /*
-      因为uip_input();函数调用了回调函数UIP_APPCALL(),即tcp_server_appcall
-      当上面的函数执行后，如果回调函数需要发送数据，则全局变量 uip_len > 0
-      需要发送的数据在uip_buf, 长度是uip_len  (这是2个全局变量)
-      */
-      if (uip_len > 0) 
-      {      //有带外回应数据
-        uip_arp_out();      //加以太网头结构，在主动连接时可能要构造ARP请求
-        tapdev_send();      //发送数据到以太网（设备驱动程序）
-      }
-    }
-    /* 处理arp报文 */
-    else if (BUF->type == htons(UIP_ETHTYPE_ARP)) 
-    { //是ARP请求包
-      uip_arp_arpin();        //如是是ARP回应，更新ARP表；如果是请求，构造回应数据包
-      /*
-      当上面的函数执行后，如果需要发送数据，则全局变量 uip_len > 0
-      需要发送的数据在uip_buf, 长度是uip_len  (这是2个全局变量)
-      */
-      if (uip_len > 0) 
-      {      //是ARP请求，要发送回应
-        tapdev_send();      //发ARP回应到以太网上
-      }
-    }
-  }
-}
-
-/*******************************************************************************
-*   函数名：InitNet
-*   输  入:
-*   输  出:
-*   功能说明：初始化网络硬件、UIP协议栈、配置本机IP地址
-************************************************************/
-void uIP_Net_Init(void)
-{
 }
