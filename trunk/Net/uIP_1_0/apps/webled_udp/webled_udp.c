0604841d6fb354955697012d76e8e8f4a85768c5
zhouq3132@gmail.com <zhouq3132@gmail.com@b410fea8-01d2-c24c-cfab-e52b0a45e8ad>
2013-01-27 13:01:07 +0000

    增加查询和设置命令，并简单测试通过

--- trunk/Net/uIP_1_0/apps/webled_udp/webled_udp.c
+++ trunk/Net/uIP_1_0/apps/webled_udp/webled_udp.c
@@ -32,8 +32,8 @@ void WebLED_UDP_APPCALL(void)
             udp_send_len = 0;
  //           u16_t len = uip_datalen();
             char *dataptr = (char *)uip_appdata;
-            if(*(int *)(dataptr+1) != NodeAttr_GetID())  //目的ID和本节点ID不同
-              break;
+            if((*(int *)(dataptr+1) != NodeAttr_GetID()) && (0xff != *(int *)(dataptr+1)))  
+              break;        //目的ID和本节点ID不同,且不为广播ID
             switch(*dataptr){
             case 0x10:
               break;
@@ -55,25 +55,99 @@ void WebLED_UDP_APPCALL(void)
               uip_send(udp_send_buf,udp_send_len);
               break;
             case 0x13:
+              udp_send_buf[0] = dataptr[0];
+              CopyMemory(&udp_send_buf[1],&dataptr[5],4);
+              CopyMemory(&udp_send_buf[5],&dataptr[1],4);
+              udp_send_buf[9] = Group_Match_Table.size;
+              CopyMemory(&udp_send_buf[10],Group_Match_Table.table,
+                         sizeof(*Group_Match_Table.table)*Group_Match_Table.size);
+              udp_send_len = sizeof(*Group_Match_Table.table)*Group_Match_Table.size + 10;
+              uip_send(udp_send_buf,udp_send_len);
               break;
             case 0x14:
+              udp_send_buf[0] = dataptr[0];
+              CopyMemory(&udp_send_buf[1],&dataptr[5],4);
+              CopyMemory(&udp_send_buf[5],&dataptr[1],4);
+              CopyMemory(&udp_send_buf[9],&node_info.mode,sizeof(node_info.mode));
+              udp_send_len = sizeof(node_info.mode) + 9;
+              uip_send(udp_send_buf,udp_send_len);
               break;
             case 0x15:
+              udp_send_buf[0] = dataptr[0];
+              CopyMemory(&udp_send_buf[1],&dataptr[5],4);
+              CopyMemory(&udp_send_buf[5],&dataptr[1],4);
+              CopyMemory(&udp_send_buf[9],&node_info.status,sizeof(node_info.status));
+              udp_send_len = sizeof(node_info.status) + 9;
+              uip_send(udp_send_buf,udp_send_len);
               break;
             case 0x16:
               break;
+              
+            case 0x20:       
+              break;
+            case 0x21:
+              udp_send_buf[0] = dataptr[0];
+              CopyMemory(&udp_send_buf[1],&dataptr[5],4);
+              CopyMemory(&udp_send_buf[5],&dataptr[1],4);
+              NodeAttr_SetID(*(int*)(dataptr+9));
+              udp_send_buf[9] = 0;
+              udp_send_len = 10;
+              uip_send(udp_send_buf,udp_send_len);              
+              break;
+            case 0x22:       
+              udp_send_buf[0] = dataptr[0];
+              CopyMemory(&udp_send_buf[1],&dataptr[5],4);
+              CopyMemory(&udp_send_buf[5],&dataptr[1],4);
+              NodeAttr_SetName(&dataptr[10],dataptr[9]);
+              udp_send_buf[9] = 0;
+              udp_send_len = 10;
+              uip_send(udp_send_buf,udp_send_len);     
+              break;
+            case 0x23:
+              udp_send_buf[0] = dataptr[0];
+              CopyMemory(&udp_send_buf[1],&dataptr[5],4);
+              CopyMemory(&udp_send_buf[5],&dataptr[1],4);
+              for(int i=0;i<dataptr[9];i++)  //循环依次添加场景
+              {
+                GroupTable_Add(*(int *)dataptr[10+4*i]);
+              }
+              udp_send_buf[9] = 0;
+              udp_send_len = 10;
+              uip_send(udp_send_buf,udp_send_len);   
+              break;
+            case 0x24:
+              udp_send_buf[0] = dataptr[0];
+              CopyMemory(&udp_send_buf[1],&dataptr[5],4);
+              CopyMemory(&udp_send_buf[5],&dataptr[1],4);
+              GroupTable_Add(*(int *)dataptr[9]);
+              udp_send_buf[9] = 0;
+              udp_send_len = 10;
+              uip_send(udp_send_buf,udp_send_len);   
+              break;
+            case 0x25:
+              udp_send_buf[0] = dataptr[0];
+              CopyMemory(&udp_send_buf[1],&dataptr[5],4);
+              CopyMemory(&udp_send_buf[5],&dataptr[1],4);
+              GroupTable_Del(*(int *)dataptr[9]);
+              udp_send_buf[9] = 0;
+              udp_send_len = 10;
+              uip_send(udp_send_buf,udp_send_len);   
+              break;
+            case 0x26:
+              udp_send_buf[0] = dataptr[0];
+              CopyMemory(&udp_send_buf[1],&dataptr[5],4);
+              CopyMemory(&udp_send_buf[5],&dataptr[1],4);
+              NodeAttr_SetMode(*(int *)dataptr[9]);
+              udp_send_buf[9] = 0;
+              udp_send_len = 10;
+              uip_send(udp_send_buf,udp_send_len);   
+              break;
+            
             default:
               break;
+            }
           }
-          if(uip_poll())
-          {
-          }
-          break;
-          
-        default:
-          break;
         }
-  }
 }
 
 void GetInfo_Server()
